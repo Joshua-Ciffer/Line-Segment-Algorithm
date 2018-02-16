fbad161d86ac9cd8e4afa8bbab4abb18ec9c7dad
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-02-16 18:14:18 -0500

    Build sketch around algorithim and point files.

--- js/Sketch.js
+++ js/Sketch.js
@@ -1,13 +1,27 @@
 /**
  * 
  * @author Joshua Ciffer
- * @version 02/15/2018
+ * @version 02/16/2018
  */
+var points = [];
+var numPoints = 0;
 
 function setup() {
-	
+	createCanvas(500, 500);
 }
 
 function draw() {
-	
+	background(0);
+	for (let i = 0; i < points.length; i++) {
+		points[i].drawPoint();
+	}
+	drawLines();
+}
+
+function mousePressed() {
+	points[numPoints++] = new Point(createVector(mouseX, mouseY));
+}
+
+function keyPressed() {
+	constructLineSegments(points);
 }
