81e65ce48639833cac54859a37708585479a4bdf
Joshua <Joshua_Ciffer@Outlook.com>
2018-02-25 17:30:07 -0500

    Moved field variables for points to point.js, wrote in proper keyPressed() function, and wrote documentation.

--- js/Sketch.js
+++ js/Sketch.js
@@ -1,27 +1,45 @@
 /**
+ * This file contains the main setup() and draw() functions to create the sketch.
  * 
  * @author Joshua Ciffer
- * @version 02/16/2018
+ * @version 02/22/2018
  */
-var points = [];
-var numPoints = 0;
 
+/**
+ * Runs once to create the canvas on the web page.
+ * 
+ * @returns void
+ */
 function setup() {
 	createCanvas(500, 500);
 	frameRate(60);
 }
 
+/**
+ * Loops repeatedly and updates the canvas with whatever is drawn.
+ * 
+ * @returns void
+ */
 function draw() {
 	background(0);
-	for (let i = 0; i < points.length; i++) {
-		points[i].drawPoint();
-	}
-	if (keyIsPressed === true) {
-		constructLineSegments(points);
-	}
-	drawLines();
+	drawPoints();	// Draws all of the points that have been created.
+	drawLines();	// Draws any line segments that connect any points.
 }
 
+/**
+ * Creates a new point wherever the user clicks.
+ * 
+ * @returns void
+ */
 function mousePressed() {
-	points[numPoints++] = new Point(createVector(mouseX, mouseY));
+	createPoint(createVector(mouseX, mouseY));
+}
+
+/**
+ * Runs the algorithm to construct any line segments when a key is pressed.
+ * 
+ * @returns void
+ */
+function keyPressed() {
+	constructLineSegments(points);
 }
